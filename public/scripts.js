f4c83971c2b5ec6d0260a50b1a5f6b2832c529f0
Pedro <pedrof.salim@gmail.com>
2020-06-10 01:53:08 -0300

    assets/styles/scripts

--- public/scripts.js
+++ public/scripts.js
@@ -1,21 +1,27 @@
-const modalOverlay = document.querySelector('.modal-overlay')
-const cards = document.querySelectorAll('.card')
+const cards = document.querySelectorAll('.card');
 
-for (let card of cards) {
+
+
+for(let card of cards) {
     card.addEventListener("click", function() {
-        const imageId = card.getAttribute('id')
-        const title = card.querySelector('.title').innerHTML
-        const author = card.querySelector('.author').innerHTML
-        
-
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector('img').src = `/assets/${imageId}.png`
-        modalOverlay.querySelector('.modal-title').innerHTML = title
-        modalOverlay.querySelector('.modal-author').innerHTML = author
-        
+        const recipeIndex = card.getAttribute("id")
+        window.location.href = `/recipe/${recipeIndex}`
     })
 }
 
-document.querySelector('.close-modal').addEventListener('click', function() {
-    modalOverlay.classList.remove('active')
-})
+const contentList = document.querySelectorAll('.content_list')
+
+for (let attribute of contentList) {
+    const button = attribute.querySelector('p')
+    const content = attribute.querySelector('.card_content')
+    button.addEventListener("click", function(){
+        if (button.innerHTML == "MOSTRAR") {
+            content.classList.add('show_content')
+            button.innerHTML = "ESCONDER"
+        }
+        else if (button.innerHTML == "ESCONDER") {
+            content.classList.remove('show_content')
+            button.innerHTML = "MOSTRAR"
+        }
+    })
+}
