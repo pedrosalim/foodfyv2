57dcb23fda98ee9efed15c72f196227b984ad58a
Pedro <pedrof.salim@gmail.com>
2020-06-28 23:40:06 -0300

    routes

--- routes.js
+++ routes.js
@@ -1,40 +1,19 @@
 const express = require('express')
 const routes = express.Router()
-const recipes = require('./data')
+const recipes = require('./controllers/recipes')
+const public = require('./controllers/public')
 
 /* >>>> ROUTES PRINCIPAIS <<<< */
-routes.get("/", function(req, res) {
-    return res.render("home", {recipes})
-})
-routes.get("/about", function(req, res) {
-    return res.render("about")
-})
-routes.get("/recipes", function(req, res) {
-    return res.render("recipes", {recipes})
-})
-routes.get("/recipe/:index", function(req, res) {
-    const recipeIndex = req.params.index
-    const recipe = recipes[recipeIndex]
- 
-    return res.render("recipe", {recipe})
-})
+routes.get("/", public.home)
+routes.get("/about", public.about)
+routes.get("/recipes", public.recipes)
+routes.get("/recipe/:index", public.recipe)
 
 
 /* >>>> ADMIN <<<< */
 
-routes.get("/admin/recipes/index", function(req, res) {
-    return res.render("admin/index", {recipes})
-})
-/*
-routes.get("/admin/recipes", recipes.index);
+routes.get("/admin/recipes/index", recipes.index)
 
-routes.get("/admin/recipes/create", recipes.create)
-routes.post("/admin/recipes/create", recipes.post)
-
-routes.get("/admin/recipes/:id", recipes.show)
-routes.get("/admin/recipes/:id/edit", recipes.edit)
-
-routes.put("/admin/recipes", recipes.put)
-routes.delete("/admin/recipes", recipes.delete)*/
+routes.get("/admin/recipes/show", recipes.show)
 
 module.exports = routes
