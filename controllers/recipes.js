1f90d92b8419a4637f6f02c022241f78484f038d
Pedro Salim <pedrof.salim@gmail.com>
2020-07-29 19:13:12 -0300

    controllers

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,4 +1,6 @@
 const data = require('../data.json')
+// const { FileSystemLoader } = require('nunjucks')
+const fs = require('fs')
 
 exports.index = function(req, res) {
     return res.render("admin/index", {recipes: data.recipes})
@@ -29,5 +31,36 @@ exports.create = function(req, res) {
 }
 
 exports.post = function(req, res) {
-    return res.send("OK")
+    const keys = Object.keys(req.body)
+
+    for (key of keys) {
+        if (req.body[key] == "") {
+            return res.send("Preencha todos os campos!!")
+        }
+    }
+
+    let { image_url, name, author, ingredients, preparations, information } = req.body
+
+    let id = 1
+    const lastRecipes = data.recipes[data.recipes.length - 1]
+
+    if (lastRecipes) {
+        id = lastRecipes.id + 1
+    }
+
+    data.recipes.push({
+        id,
+        image_url,
+        name,
+        author,
+        ingredients,
+        preparations,
+        information
+    })
+    
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) return res.send("Houve um erro")
+
+        return res.redirect("/admin/recipes/index")
+    })
 }
