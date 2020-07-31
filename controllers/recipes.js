c25cf0c8b7748af426fc5a3e502599fe30ff76d5
Pedro Salim <pedrof.salim@gmail.com>
2020-07-30 23:56:14 -0300

    controllers

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -1,6 +1,6 @@
+const fs = require('fs')
 const data = require('../data.json')
 // const { FileSystemLoader } = require('nunjucks')
-const fs = require('fs')
 
 exports.index = function(req, res) {
     return res.render("admin/index", {recipes: data.recipes})
@@ -30,37 +30,54 @@ exports.create = function(req, res) {
     return res.render("admin/create")
 }
 
-exports.post = function(req, res) {
-    const keys = Object.keys(req.body)
+/* exports.post = function(req, res) {
+    const allRecipes = data.recipes
 
-    for (key of keys) {
-        if (req.body[key] == "") {
-            return res.send("Preencha todos os campos!!")
-        }
-    }
+    let { image_url, title, author, preparation, information, ingredients } = req.body
 
-    let { image_url, name, author, ingredients, preparations, information } = req.body
-
-    let id = 1
-    const lastRecipes = data.recipes[data.recipes.length - 1]
-
-    if (lastRecipes) {
-        id = lastRecipes.id + 1
-    }
+    const id = Number(data.recipes.length + 1)
 
     data.recipes.push({
         id,
-        image_url,
-        name,
-        author,
-        ingredients,
-        preparations,
-        information
+        ...req.body
     })
     
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
         if (err) return res.send("Houve um erro")
 
+        return res.render("admin/recipes", { allRecipes })
+    })
+} */
+
+exports.post = function(req, res) {
+    const keys = Object.keys(req.body).pop()
+
+    for(key of keys) {
+        if(req.body[key] == "") {
+            return res.send("Preencha todos os campos!")
+        }
+    }
+
+    let { image_url, title, author, ingredients, preparation, information } = req.body
+    const lastRecipe = data.recipes[data.recipes.length - 1]
+
+    if(lastRecipe) {
+        id = lastRecipe.id + 1
+    }
+    data.recipes.push({
+        id,
+        image_url,
+        title,
+        author,
+        ingredients,
+        preparation,
+        information
+    })
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Houve um erro")
+
         return res.redirect("/admin/recipes/index")
     })
+
 }
