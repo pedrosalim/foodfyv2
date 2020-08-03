06831d6683511baad1f16440b1a65fb9f46e8ac6
Pedro Salim <pedrof.salim@gmail.com>
2020-08-02 23:06:28 -0300

    controllers

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -107,3 +107,20 @@ exports.put = function(req, res){
         return res.redirect(`recipes/${id}`)
     })
 }
+
+exports.delete = function(req, res) {
+    const { id } = req.body
+
+    const filteredRecipes = data.recipes.filter(function(recipe) {
+        return recipe.id != id
+
+    })
+
+    data.recipes = filteredRecipes
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if(err) return res.send("Houve um erro!")
+
+        return res.redirect("/admin/recipes/index")
+    })
+}
